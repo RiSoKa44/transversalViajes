991b7496d380cb45ee25ba0c8f51c6072973a24e
RiSoKa44 <a14javsuaarc@inspedralbes.cat>
2019-12-04 16:11:44 +0100

    Optimizado en un 50% el peso de la imagen Gift de Index.php

--- site_media/html/index.php
+++ site_media/html/index.php
@@ -12,7 +12,7 @@
         <h2 class="welcomeweb">Bienvenido al rincón del viajero</h2>
         <h3 class="info">Lugar donde podrás compartir tus experiencias de viajes con muchas otras personas que aman viajar igual que tu.
 </h3>
-        <img class="imganimated" src="../img/imagen-animada-cabezera-viajesdaw.gif">
+        <img class="imganimated" src="../img/imagen-animada-cabezera-viajesdaw-opt.gif">
         <h4 class="moreinfo">Comparte tus viajes, lee las experiencias de otros usuarios, valora para que aquellos indecisos tengan ideas de donde poder viajar, qué nuevo lugar descubrir.
 </h4> 
     </div>
