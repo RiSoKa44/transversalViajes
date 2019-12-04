1b464ceb45ed957a54c7ce02a83c7fca0637c2fe
RiSoKa44 <a14javsuaarc@inspedralbes.cat>
2019-12-04 15:39:52 +0100

    Creado header provisional, footer, index. Imagen animada, letra personalizada

---
+++ site_media/html/header.php
@@ -0,0 +1,64 @@
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Viajes DAW</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" type="image/png" href="img/favicon.ico"/>
+
+    <!--LINKS EXTERNOS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- CSS LINKS -->
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/header.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/index.css'>
+
+    <!-- SCRIPTS EXTERNOS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+
+    <!-- JS SCRIPTS -->
+
+    
+    <!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+          <img src="../img/logoTransversal2.png" alt="logo-transversal2-viajesdaw">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Inicio
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categorias
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Aventura</a>
+          <a class="dropdown-item" href="#">Deporte</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Montaña</a>
+        </div>
+      </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mi Experiencia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">MI PERFIL</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</head>
---
+++ site_media/html/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<?php
+    include 'header.php';
+?>
+
+<body>
+<div class="container">
+    <div class="headindex">
+        <h1 class="tituloweb"></h1>
+        <h2 class="welcomeweb">Bienvenido al rincón del viajero</h2>
+        <h3 class="info">Lugar donde podrás compartir tus experiencias de viajes con muchas otras personas que aman viajar igual que tu.
+</h3>
+        <img class="imganimated" src="../img/imagen-animada-cabezera-viajesdaw.gif">
+        <h4 class="moreinfo">Comparte tus viajes, lee las experiencias de otros usuarios, valora para que aquellos indecisos tengan ideas de donde poder viajar, qué nuevo lugar descubrir.
+</h4> 
+    </div>
+</div>
+</body>
+
+<?php
+    include 'footer.php';
+?>
+
+</html>
